08a5b2f3476723ea72dccb9aa4128d76abe05552
Cibele Martins <cibelemdev@gmail.com>
2024-09-07 16:52:42 -0300

    💄 🚧 feat: add button e tema personalizado

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
+import { Button } from "native-base";
+import { GestureResponderEvent } from 'react-native';
+
+type ButtonProps = PropsWithChildren<{
+  title: string;
+  onPress: (e: GestureResponderEvent)=> void;
+  size: string;
+  variant: string;
+}>;
 
 
-
-export default function Button({children, title}: SectionProps): React.JSX.Element {
+export default function ButtonUI({title, onPress, size, variant}: ButtonProps): React.JSX.Element {
 
     return (
-
-        )
+         <Button backgroundColor={'primary.50'} size={size} variant={variant} onPress={onPress}>{title}</Button>
+    )
 
 }
