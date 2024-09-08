9e7f799cc39e22909d89a4166f5fa8346e4e6a2c
Cibele Martins <cibelemdev@gmail.com>
2024-09-08 20:04:49 -0300

    🚧 ♻️ refactor: add Center no componente global withGradient, removeu headers das rotas

--- src/components/wrapperGradient/index.tsx
+++ src/components/wrapperGradient/index.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { LinearGradient } from 'react-native-linear-gradient';
 import { SafeAreaView } from 'react-native';
 import NavbarMenuUI from '../navBarMenu';
-import { HStack } from 'native-base';
+import { Center, HStack } from 'native-base';
+import { useRoute } from '@react-navigation/native';
 
 export default function withGradient(WrappedComponent: any) {
+  
     return function (props: React.JSX.IntrinsicAttributes) {
+        const route = useRoute();
+        const isLoginRoute = route?.name === 'Login';
         return (
             <LinearGradient
                 colors={['#1A00BA', '#585865']}
@@ -13,8 +17,11 @@ export default function withGradient(WrappedComponent: any) {
             >
                 <SafeAreaView style={{ flex: 1 }}>
                     <HStack>
-                        <NavbarMenuUI />
+                        {!isLoginRoute && <NavbarMenuUI />}
+                        <Center flexDir={'row'}>
                         <WrappedComponent {...props} />
+                        </Center>
+                     
                     </HStack>
                 </SafeAreaView>
             </LinearGradient>
--- src/navigator/AppNavigator.js
+++ src/navigator/AppNavigator.js
@@ -15,19 +15,23 @@ const Stack = createNativeStackNavigator();
 // Envolva suas telas com o gradiente
 const GradientLogin = withGradient(Login);
 const GradientHome = withGradient(Home);
-
+const GradientRegisterCustomer = withGradient(RegisterCustomer);
+const GradientRegisterAdvertisement = withGradient(RegisterAdvertisement);
+const GradientEdit = withGradient(Edit);
+const GradientVisualization = withGradient(Visualization);
+const GradientDashboard = withGradient(Dashboard);
+const GradientBillingInformation = withGradient(BillingInformation);
 export default function Navigator() {
     return (
         <Stack.Navigator initialRouteName='Login'>
-            <Stack.Screen name="Login" component={GradientLogin} />
-            {/* <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Cadastrar cliente" component={RegisterCustomer} />
-            <Stack.Screen name="Cadastrar propagandas" component={RegisterAdvertisement} />
-            <Stack.Screen name="editar/cliente" component={Edit} />
-            <Stack.Screen name="editar/propagandas" component={Edit} />
-            <Stack.Screen name="visualizar/propagandas e clientes" component={Visualization} />
-            <Stack.Screen name="/dashboard" component={Dashboard} />
-            <Stack.Screen name="propagandas/informações/cobrança" component={BillingInformation} /> */}
+            <Stack.Screen options={{ headerShown: false }} name="Login" component={GradientLogin} />
+            <Stack.Screen options={{ headerShown: false }} name="Home" component={GradientHome} />
+            <Stack.Screen options={{ headerShown: false }} name="Cadastrar cliente" component={GradientRegisterCustomer} />
+            <Stack.Screen options={{ headerShown: false }} name="Cadastrar propaganda" component={GradientRegisterAdvertisement} />
+            <Stack.Screen options={{ headerShown: false }} name="editar/cliente e propaganda" component={GradientEdit} />
+            <Stack.Screen options={{ headerShown: false }} name="visualizar/propagandas e clientes" component={GradientVisualization} />
+            <Stack.Screen options={{ headerShown: false }} name="Dashboard" component={GradientDashboard} />
+            <Stack.Screen options={{ headerShown: false }} name="Informações Cobrança Propaganda" component={GradientBillingInformation} />
         </Stack.Navigator>
     );
 }
