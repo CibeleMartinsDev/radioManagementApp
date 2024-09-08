792648755f91550053afb07ddcef5994428a02dd
Cibele Martins <cibelemdev@gmail.com>
2024-09-08 20:05:21 -0300

    🚧 feat: estrutura básica página de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,36 @@
 import { useNavigation } from "@react-navigation/native";
-import { Button, Text, View } from "native-base"; // Certifique-se de ter o NativeBase configurado corretamente
+import { Button, Image, Text, View, VStack } from "native-base"; // Certifique-se de ter o NativeBase configurado corretamente
+import DynamicFormUI from "../../components/dynamicForm";
+import ButtonUI from "../../components/button";
+import { GestureResponderEvent } from "react-native";
+import { useState } from "react";
 
 export default function Login() {
     const navigation = useNavigation()
+    const [emailUser, setEmailUser] = useState('');
+    const [passwordUser, setPasswordUser] = useState('');
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Login Screen</Text>
-            <Button onPress={() => navigation.navigate('Home' as never)}>Go to Home</Button>
-        </View>
+        <VStack space={20} w={'100%'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
+            <Image size={140} position={'fixed'} top={140} source={require('../../assets/logoApp.png')} />
+            <DynamicFormUI 
+            height="customHeightNavbarMenu"
+            inputs={[
+                {
+                    type: 'text',
+                    name: 'email',
+                    placeholder: 'E-mail',
+                    value: emailUser,
+                    onChange: (e) => { setEmailUser(e.nativeEvent.text) }
+                },
+                {
+                    type: 'text',
+                    name: 'password',
+                    placeholder: 'Senha',
+                    value: passwordUser,
+                    onChange: (e) => { setPasswordUser(e.nativeEvent.text) }
+                },
+            ]} theme={"customer"} />
+            <ButtonUI title={"Entrar"} onPress={()=> navigation.navigate('Home' as never)} size={""} variant={""} />
+        </VStack>
     );
 }
