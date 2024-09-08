a1d8e8d9b87ea968c9b419b1aca1fd978b9c56ff
Cibele Martins <cibelemdev@gmail.com>
2024-09-08 20:05:53 -0300

    🚧 feat: estrutura básica página de cadastro de propaganda

--- src/pages/registerAdvertisement/index.tsx
+++ src/pages/registerAdvertisement/index.tsx
@@ -1,7 +1,74 @@
 import { Text } from "react-native";
+import DynamicFormUI from "../../components/dynamicForm";
+import ButtonUI from "../../components/button";
+import { useState } from "react";
+import { InputDynamicForm } from "../../components/dynamicForm/interface";
 
-export default function RegisterAdvertisement(){
+export default function RegisterAdvertisement() {
+
+    const [nameCustomer, setNameCustomer] = useState('')
+    const [adressCustomer, setAdressCustomer] = useState('')
+    const [phone1Customer, setPhone1Customer] = useState('')
+    const [phone2Customer, setPhone2Customer] = useState('')
+    const [emailCustomer, setEmailCustomer] = useState('')
+    const [selectValue, setSelectValue] = useState('')
+
+    const inputsAdvertisement: InputDynamicForm[] = [
+        {
+            type: 'text',
+            name: 'name',
+            placeholder: 'Nome da propaganda',
+            value: nameCustomer,
+            onChange: (e) => { setNameCustomer(e.nativeEvent.text) }
+        },
+        {
+            type: 'text',
+            name: 'frenquency',
+            placeholder: 'Frequência de divulgação',
+            value: adressCustomer,
+            onChange: (e) => { setAdressCustomer(e.nativeEvent.text) }
+        },
+        {
+            type: 'text',
+            name: 'hours',
+            placeholder: 'Horários de divulgação',
+            value: phone1Customer,
+            onChange: (e) => { setPhone1Customer(e.nativeEvent.text) }
+        },
+        {
+            type: 'text',
+            name: 'amount',
+            placeholder: 'R$ Valor',
+            value: phone2Customer,
+            onChange: (e) => { setPhone2Customer(e.nativeEvent.text) }
+        },
+        {
+            type: 'text',
+            name: 'dateDue',
+            placeholder: 'Data de vencimento',
+            value: emailCustomer,
+            onChange: (e) => { setEmailCustomer(e.nativeEvent.text) }
+        },
+        {
+            type: 'text',
+            name: 'activate',
+            placeholder: 'Ativar propaganda?',
+            value: emailCustomer,
+            onChange: (e) => { setEmailCustomer(e.nativeEvent.text) }
+        },
+    ]
+
     return (
-        <Text>PAGE BillingInformation</Text>
+        <DynamicFormUI selectOptions={{
+            value: selectValue,
+            onChange: (e)=> setSelectValue(e),
+            placeholder: 'Selecione o cliente',
+            items: [
+                {label: 'Selecione o cliente', value: ''},
+                {label: 'Lojas MM', value: 'Lojas MM'}
+            ]
+        }} inputs={inputsAdvertisement} theme={"advertisement"} height={""} >
+            <ButtonUI title={"Cadastrar propaganda"} onPress={() => { console.log('cadastrar cliente') }} size={"100%"} variant={"ghost"} />
+        </DynamicFormUI>
     )
 }
