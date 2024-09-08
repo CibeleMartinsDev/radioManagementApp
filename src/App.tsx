4177c2269dc80ef335efc4cba848d5b91b961333
Cibele Martins <cibelemdev@gmail.com>
2024-09-08 20:08:24 -0300

    ♻️ refactor: removeu comentários e organizou importações

--- src/App.tsx
+++ src/App.tsx
@@ -6,136 +6,16 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import {
-  GestureResponderEvent,
-  Image,
-  NativeSyntheticEvent,
-  SafeAreaView,
-  TextInputChangeEventData,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-
-//libs
-import { LinearGradient } from 'react-native-linear-gradient';
-import { AspectRatio, Center, HStack, NativeBaseProvider, useDisclose } from "native-base";
-
-//components/themes
-import { theme } from './styles/theme/nativeBaseTheme'
-import NavbarMenuUI from './components/navBarMenu';
-import CardVisualizationUI from './components/cardVisualization';
-import Navigator from './navigator/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 import { enableScreens } from 'react-native-screens';
-import Login from './pages/login';
-import ButtonUI from './components/button';
+import {  NativeBaseProvider } from "native-base";
+import { theme } from './styles/theme/nativeBaseTheme'
+import Navigator from './navigator/AppNavigator';
 
 enableScreens();
 
 function App(): React.JSX.Element {
-
-
-  // const [nameCustomer, setNameCustomer] = useState('')
-  // const [adressCustomer, setAdressCustomer] = useState('')
-  // const [phone1Customer, setPhone1Customer] = useState('')
-  // const [phone2Customer, setPhone2Customer] = useState('')
-  // const [emailCustomer, setEmailCustomer] = useState('')
-  // const [selectValue, setSelectValue] = useState('')
-
-  // const inputs: InputDynamicForm[] = [
-  //   {
-  //     type: 'text',
-  //     name: 'name',
-  //     placeholder: 'Nome do cliente',
-  //     value: nameCustomer,
-  //     onChange: (e) => { setNameCustomer(e.nativeEvent.text) }
-  //   },
-  //   {
-  //     type: 'text',
-  //     name: 'endereco',
-  //     placeholder: 'Endereço do cliente',
-  //     value: adressCustomer,
-  //     onChange: (e) => { setAdressCustomer(e.nativeEvent.text) }
-  //   },
-  //   {
-  //     type: 'text',
-  //     name: 'phone1',
-  //     placeholder: 'Telefone 1',
-  //     value: phone1Customer,
-  //     onChange: (e) => { setPhone1Customer(e.nativeEvent.text) }
-  //   },
-  //   {
-  //     type: 'text',
-  //     name: 'phone2',
-  //     placeholder: 'Telefone 2',
-  //     value: phone2Customer,
-  //     onChange: (e) => { setPhone2Customer(e.nativeEvent.text) }
-  //   },
-  //   {
-  //     type: 'text',
-  //     name: 'email',
-  //     placeholder: 'E-mail',
-  //     value: emailCustomer,
-  //     onChange: (e) => { setEmailCustomer(e.nativeEvent.text) }
-  //   },
-  // ]
-
-  // const inputsAdvertisement: InputDynamicForm[] = [
-  //   {
-  //     type: 'text',
-  //     name: 'name',
-  //     placeholder: 'Nome da propaganda',
-  //     value: nameCustomer,
-  //     onChange: (e) => { setNameCustomer(e.nativeEvent.text) }
-  //   },
-  //   {
-  //     type: 'text',
-  //     name: 'frenquency',
-  //     placeholder: 'Frequência de divulgação',
-  //     value: adressCustomer,
-  //     onChange: (e) => { setAdressCustomer(e.nativeEvent.text) }
-  //   },
-  //   {
-  //     type: 'text',
-  //     name: 'hours',
-  //     placeholder: 'Horários de divulgação',
-  //     value: phone1Customer,
-  //     onChange: (e) => { setPhone1Customer(e.nativeEvent.text) }
-  //   },
-  //   {
-  //     type: 'text',
-  //     name: 'amount',
-  //     placeholder: 'R$ Valor',
-  //     value: phone2Customer,
-  //     onChange: (e) => { setPhone2Customer(e.nativeEvent.text) }
-  //   },
-  //   {
-  //     type: 'text',
-  //     name: 'dateDue',
-  //     placeholder: 'Data de vencimento',
-  //     value: emailCustomer,
-  //     onChange: (e) => { setEmailCustomer(e.nativeEvent.text) }
-  //   },
-  //   {
-  //     type: 'text',
-  //     name: 'activate',
-  //     placeholder: 'Ativar propaganda?',
-  //     value: emailCustomer,
-  //     onChange: (e) => { setEmailCustomer(e.nativeEvent.text) }
-  //   },
-  // ]
-
-  // const onSubmit = () => {
-  //   console.log(nameCustomer),
-  //     console.log(adressCustomer),
-  //     console.log(emailCustomer),
-  //     console.log(phone1Customer)
-  //   console.log(phone2Customer)
-  // }
   return (
 
     <NativeBaseProvider theme={theme}>
