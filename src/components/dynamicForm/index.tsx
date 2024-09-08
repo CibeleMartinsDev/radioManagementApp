d2d2c1285394c787189190ca4f79bd90c75c3024
Cibele Martins <cibelemdev@gmail.com>
2024-09-08 20:03:53 -0300

    ♻️ refactor: removi margin bottom e importações ñ utilizadas no DynamicFormUI

--- src/components/dynamicForm/index.tsx
+++ src/components/dynamicForm/index.tsx
@@ -1,20 +1,15 @@
 import { Box, FormControl, Input, VStack } from "native-base";
 import { DynamicFormUIProps } from "./interface";
 import { InputUI } from "../input";
-import { NativeSyntheticEvent, TextInputChangeEventData } from "react-native";
-import ButtonUI from "../button";
-import { Children, useEffect, useState } from "react";
-import * as Yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { Controller, useForm } from "react-hook-form";
+
 import SelectUI from "../select";
 import { Items } from "../select/interface";
 
 
-export default function DynamicFormUI({ inputs, children, theme, selectOptions }: DynamicFormUIProps) {
+export default function DynamicFormUI({ inputs, children, theme, selectOptions, height }: DynamicFormUIProps) {
 
     return (
-        <VStack mt={40} display="flex" justifyContent={'center'} alignItems={'center'} space={20} height={'customHeightDynamicForm'} width="70%" mx="3">
+        <VStack display="flex" justifyContent={'center'} alignItems={'center'} space={20} height={height ? height : 'customHeightDynamicForm'} width="70%" mx="3">
 
             {theme === 'customer' ? inputs.map(i => {
                 return (
